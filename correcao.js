f8470f11a6fb436cd0a3e3400e77a8235a0514f0
Jorge-Bernardi <jorgeguilhermebernardi@gmail.com>
2024-09-06 10:07:40 -0300

    fix: code correction

--- correcao.js
+++ correcao.js
@@ -1,25 +1,16 @@
 // Jorge Guilherme Bernardi - 2° Semestre DSM
 
-// 1° Erro: Declaração de Variáveis
+
 // Correção das variavéis
 
 const y = 5;
 let x = 5;
 x += 10
 
-//Criação da função para realizar a soma...
-
 function somar() {
-
-  //2° Erro: Formulação da conta, faltava o 'const' 
-  // Calculando a soma
-
-  const r = x + y;
-
-  //3° Erro: Não estava retornando o resultado da variável
-  // Retornando o resultado
   
-  return r;
+  return  x + y; 
+
 }
 
 // Exibindo o resultado no console
